cce5be884e1c210acf246a9269a09663c1ebd5b7
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-09-02 01:45:48 +0900

    9/1 login part2

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting=document.querySelector("#greeting");
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  const username=loginInput.value;
+  loginForm.classList.add("hidden");
+  greeting.innerHTML=`Hello ${username}`;
+  greeting.classList.remove("hidden");
 }
 loginForm.addEventListener("submit",onLoginSubmit);
