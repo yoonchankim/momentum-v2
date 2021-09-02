f164adcd75008a40ab4fa5a98bd929c08f45c36f
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-09-02 12:01:53 +0900

    9/2 login part3

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const loginInput = document.querySelector("#login-form input");
 const greeting=document.querySelector("#greeting");
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username=loginInput.value;
   loginForm.classList.add("hidden");
+  localStorage.setItem("username",loginInput.value);
+  paintGreetings();
+}
+function paintGreetings(){
+  const username=localStorage.getItem("username");
   greeting.innerHTML=`Hello ${username}`;
   greeting.classList.remove("hidden");
 }
-loginForm.addEventListener("submit",onLoginSubmit);
+const savedUsername=localStorage.getItem("username");
+if(savedUsername===null){
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit",onLoginSubmit);
+}
+else{
+  paintGreetings();
+}
