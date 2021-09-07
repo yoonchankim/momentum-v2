df2ea0e5fd0d5ecaa1320f77e3de2f087fc3c9a1
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-09-07 20:33:34 +0900

    9/7 weather part 2 except css finsh all

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,18 @@
+const API_KEY="c900540d80c2d2ed7da160796e77500b";
 function onGeoOk(position){
     const lat=position.coords.latitude;
-    const lng=position.coords.longitude;
-    console.log(`You live in ${lat}   ${lng}`);
+    const lon=position.coords.longitude;
+    const URL=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(URL).then(response => response.json()).then(data=>{
+        const city=document.querySelector("#weather span:first-child");
+        const Weather=document.querySelector("#weather span:last-child");
+        const name=data.name;
+        const weather=`${data.weather[0].main} / ${data.main.temp}`;
+        city.innerHTML=name;
+        Weather.innerHTML=weather;
+    });
 }
 function onGeoError(){
-    alert("Can't find you.")
+    alert("Can't find you.");
 }
 navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
