e6d20685d44e2c689f8a39a6628c5955077fd3f5
yoonchankim <73942196+yoonchankim@users.noreply.github.com>
2021-09-10 23:19:22 +0900

    9/10 css
    내일 할 것
    높이 줄이면 화면 가리기(이유:명언이 이미지보다 밑에 나와서)
    명언 누르면 작가 나오게 하기(처음에 작가 안 나오게 하기)

--- js/greetings.js
+++ js/greetings.js
@@ -1,9 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting=document.querySelector("#greeting");
+const bigForm = document.querySelector("#big-form");
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add("hidden");
+  bigForm.classList.add("hidden");
   localStorage.setItem("username",loginInput.value);
   paintGreetings();
 }
@@ -11,10 +13,13 @@ function paintGreetings(){
   const username=localStorage.getItem("username");
   greeting.innerHTML=`Hello ${username}`;
   greeting.classList.remove("hidden");
+  loginForm.classList.add("hidden");
+  bigForm.classList.add("hidden");
 }
 const savedUsername=localStorage.getItem("username");
 if(savedUsername===null){
   loginForm.classList.remove("hidden");
+  bigForm.classList.remove("hidden");
   loginForm.addEventListener("submit",onLoginSubmit);
 }
 else{
